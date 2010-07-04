b2616c2a4c06fcbf98a7c75b8fa97efc648a4a9c
GDR! <gdr@D400-GDR.(none)>
2010-07-04 18:12:29 +0200

    c4 upto Welcoming Player

---
+++ tex/python/zgadula100.py
@@ -0,0 +1,38 @@
+# Gra w zgadywanie liczby
+import random
+
+iloscProb = 0
+
+print('Cześć! Jak masz na imię?')
+imie = input()
+
+liczba = random.randint(1, 100)
+print('Cześć ' + imie + '. Myślę o pewnej liczbie od 1 do 100.')
+
+while iloscProb < 6:
+    print('Zgaduj:')
+    strzal = input()
+    strzal = int(strzal)
+
+    iloscProb = iloscProb + 1
+
+    if strzal < liczba:
+        print('Strzeliłeś zbyt niską liczbę') # na początku linii jest 8 spacji
+
+    if strzal > liczba:
+        print('Strzeliłeś zbyt wysoką liczbę')
+
+    if strzal == liczba:
+        break
+
+if strzal == liczba:
+    iloscProb = str(iloscProb)
+    print('Dobra robota, ' + imie + '! Zgadłeś moją liczbę po ' + iloscProb + ' próbach!')
+
+if strzal != liczba:
+    liczba = str(liczba)
+    print('Nie. Liczba o której myślałem to ' + liczba)
+
+
+
+
